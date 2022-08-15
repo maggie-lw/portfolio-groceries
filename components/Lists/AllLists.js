2ef9742c095345f24c4bd3631619b91adc913056
Maggie Lee <maggie.lw.lee@gmail.com>
2022-08-15 21:18:03 +1000

    Added flexboxes for list page + change colors

--- components/Lists/AllLists.js
+++ components/Lists/AllLists.js
@@ -1,5 +1,29 @@
+import classes from "./AllLists.module.css";
+
+import { getAuth } from "firebase/auth";
+
+import { Fragment } from "react";
+
 const AllLists = () => {
-    return <h1>List of Lists</h1>;
+
+    const auth = getAuth();
+    const username = auth.currentUser.displayName;
+
+  return (
+    <Fragment>
+      <div className={classes.blocks}>
+        <section className={classes.sidebar}>
+          <section className={classes.user}><p>Welcome, </p><h1>{username}!</h1></section>
+          <section className={classes.list}>
+            <h1>List of lists</h1>
+          </section>
+        </section>
+        <section className={classes.listcontent}>
+          <p>List content</p>
+        </section>
+      </div>
+    </Fragment>
+  );
 };
 
 export default AllLists;
