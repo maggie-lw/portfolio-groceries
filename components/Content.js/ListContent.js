3591528770d04803d54d36d7bf25fde84f2bd6bf
Maggie Lee <maggie.lw.lee@gmail.com>
2022-08-17 15:53:00 +1000

    Grocery List CSS changes

--- components/Content.js/ListContent.js
+++ components/Content.js/ListContent.js
@@ -2,11 +2,17 @@ import classes from "./ListContent.module.css";
 
 const ListContent = (props) => {
   return (
-    <div>
-      <li className={classes.list}>
-        <h3>{props.item}</h3>
-        <h3>x {props.amount}</h3>
-      </li>
+    <div className={classes.content}>
+      <div className={classes.listarea}>
+        <li className={classes.list}>
+          <h3>{props.item}</h3>
+          <h3>x {props.amount}</h3>
+        </li>
+      </div>
+      <div className={classes.buttons}>
+        <button>Edit</button>
+        <button>Delete</button>
+      </div>
     </div>
   );
 };
